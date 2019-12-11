31f059c053f10a9c24f96450e43fea8408e6b294
ericcuys <eric.cuysona@gmail.com>
2019-12-11 11:41:22 +0800

    update/add table content

---
+++ assets/css/update.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form method="POST">
+	<div class="container">
+		Username: <input type="text" name="username" autocomplete="off" class="form-control"><br>
+		Password: <input type="password" name="password" autocomplete="off" class="form-control"><br>
+
+		<input value="Create" type="submit" name="submit" class="btn btn-success btn-xl">
+	</div>
+
+</form>
+
+
+</body>
+</html>
